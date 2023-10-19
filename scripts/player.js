7ed5a93e40a88227ab510ece030f4f2115cc0486
Muganwa <stevenmuganwa@live.com>
2023-10-19 14:34:28 +0300

    yt api fix

--- scripts/player.js
+++ scripts/player.js
@@ -2,14 +2,16 @@ const iContainer = document.getElementById('stream_container');
 const i = document.createElement('iframe');
 const m = document.createElement('span');
 m.id = "mute";
-i.src = "https://www.youtube.com/embed/C4XvJpqKJfc?start=1&autoplay=1&cc_lang_pref=en&cc_load_policy=1&controls=0&disablekb=1&fs=1&playsinline=1&rel=0&modestbranding=1&iv_load_policy=3&showinfo=0&wmode=transparent&mute=1&enablejsapi=1&origin=https%3A%2F%2Fthedentivive.com&widgetid=1";
+i.id = "tdc";
+i.src = "https://www.youtube.com/embed/C4XvJpqKJfc?enablejsapi=1&start=1&cc_lang_pref=en&cc_load_policy=1&controls=0&showinfo=0&mute=1&autoplay=1";
 i.title = "Natural Tooth Decay And Gum Disease Relief";
 i.width = "100%";
 i.height = "640";
-i.frameborder = "0";
-i.id = "tooth-decay-vid";
 i.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;";
 i.allowFullscreen = true;
+
+iContainer.appendChild(m);
+iContainer.appendChild(i);
 
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
@@ -19,28 +21,21 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('tooth-decay-vid', {
+    player = new YT.Player('tdc', {
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
         }
     });
-    iContainer.appendChild(i);
-    iContainer.appendChild(m);
 }
 
-function onPlayerReady() {
-    player.playVideo();
-
+function onPlayerReady() { // do something fancy
 }
 
-function onPlayerStateChange() {
-    console.log("my state changed");
+function onPlayerStateChange() { // do something fancy
 }
 
 document.getElementById("mute").addEventListener('click', function (event) {
-    console.log('player ', player);
-
     if (player.isMuted()) {
         player.unMute();
     } else {
